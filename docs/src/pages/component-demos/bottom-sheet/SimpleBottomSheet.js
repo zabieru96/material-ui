5f04f04e3b086c9513e9925f9754ae7a4c294209
Xavier Bryson <zabieru96@hotmail.com>
2017-07-21 09:34:50 -0500

    [Bottom Sheet added base styles and properties to display a Simple persistent and modal sheet

--- docs/src/pages/component-demos/bottom-sheet/SimpleBottomSheet.js
+++ docs/src/pages/component-demos/bottom-sheet/SimpleBottomSheet.js
@@ -7,6 +7,12 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles, createStyleSheet } from 'material-ui/styles';
 import BottomSheet from 'material-ui/BottomSheet';
+import Button from 'material-ui/Button';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import InboxIcon from 'material-ui-icons/Inbox';
+import DraftsIcon from 'material-ui-icons/Drafts';
+import StarIcon from 'material-ui-icons/Star';
+import SendIcon from 'material-ui-icons/Send';
 
 const styleSheet = createStyleSheet('SimpleBottomSheet', {
   root: {
@@ -16,12 +22,56 @@ const styleSheet = createStyleSheet('SimpleBottomSheet', {
 
 class SimpleBottomSheet extends Component {
 
+  state = {
+    open: false,
+  };
+
+  openSheet = () => {
+    this.setState({open: true});
+  };
+
+
   render() {
     const classes = this.props.classes;
 
+    const options = (
+      <div>
+        <ListItem button>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText primary="Inbox" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <StarIcon />
+          </ListItemIcon>
+          <ListItemText primary="Starred" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <SendIcon />
+          </ListItemIcon>
+          <ListItemText primary="Send mail" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <DraftsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Drafts" />
+        </ListItem>
+      </div>
+    );
+
     return (
       <div className={classes.root}>
-        <BottomSheet/>
+        <Button onClick={this.openSheet}>Open BottomSheet</Button>
+        <BottomSheet
+          open={this.state.open}
+          heading="Hello Sheet"
+        >
+          {options}
+        </BottomSheet>
       </div>
     );
   }
--- src/BottomSheet/BottomSheet.js
+++ src/BottomSheet/BottomSheet.js
@@ -2,11 +2,16 @@
  * Created by zabieru on 6/26/2017.
  */
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { createStyleSheet } from 'jss-theme-reactor';
 import withStyles from '../styles/withStyles';
 import Slide from '../transitions/Slide';
 import Paper from '../Paper';
+import Modal from '../internal/Modal';
+import { duration } from '../styles/transitions';
+import common from '../colors/common';
+import {fade} from '../styles/colorManipulator'
 
 export const styleSheet = createStyleSheet('MuiBottomSheet', {
   root: {
@@ -15,39 +20,118 @@ export const styleSheet = createStyleSheet('MuiBottomSheet', {
   paper: {
     position: 'fixed',
     bottom: 0,
-    width: '800px',
-    margin: '0 auto', /* Will not center vertically and won't work in IE6/7. */
     left: 0,
     right: 0,
   },
+  heading: {
+    'line-height': '56px',
+    display: 'inline-block',
+    'font-size': 16,
+    color: fade(common.black, 0.54),
+    width: '100%',
+    'vertical-align' : 'middle',
+    padding: '0 16px',
+    'font-family' : 'Roboto',
+  },
+  inset: {
+    width: '800px',
+    margin: '0 auto', /* Will not center vertically and won't work in IE6/7. */
+  },
+  persistent: {
+    'z-index' : 1400,
+  }
 });
 
 class BottomSheet extends Component{
+
+  static defaultProps = {
+    enterTransitionDuration: duration.enteringScreen,
+    leaveTransitionDuration: duration.leavingScreen,
+    open: false,
+    modal: false,
+  };
+
+  componentWillMount(){
+    this.setState({
+      open: this.props.open
+    })
+  }
+
+  handleClose = () => {
+    this.setState(
+      {open: false}
+    )
+  };
 
   render(){
     const {
       classes,
       className,
+      children,
+      open,
+      heading,
+      modal,
       ...other
     } = this.props;
 
-    return (
-      <Slide
-        in={true}
-        direction='up'
-        enterTransitionDuration={2}
-        leaveTransitionDuration={2}
-      >
-        <Paper
-          elevation={4}
-          square
-          className={classNames(classes.paper)}
+    // const {
+    //   open
+    // } = this.state;
+
+    const header = heading? heading : "";
+
+    if (modal) {
+      return (
+        <Modal
+          backdropTransitionDuration={open ? duration.leavingScreen : duration.leavingScreen}
+          show={open}
+          onRequestClose={this.handleClose}
         >
-          <h1>Hello Sheet</h1>
-        </Paper>
-      </Slide>
-    )
+          <Slide
+            in={open}
+            direction='up'
+            enterTransitionDuration={300}
+            leaveTransitionDuration={300}
+          >
+            <Paper
+              elevation={4}
+              square
+              className={classNames(classes.paper)}
+            >
+              <span className={classNames(classes.heading)}>{header}</span>
+              {children}
+            </Paper>
+          </Slide>
+        </Modal>
+      )
+    }else{
+      return (
+        <div>
+          <Paper
+            square
+            className={classNames(classes.paper, classes.persistent)}
+          >
+            <span className={classNames(classes.heading)}>{header}</span>
+            {children}
+          </Paper>
+        </div>
+      )
+    }
   }
 }
+
+BottomSheet.propTypes = {
+  /**
+   * Used to display an optional heading within the component.
+   */
+  heading: PropTypes.string,
+  /**
+   * Toggle to determine the type of bottom sheet. If false and by default displays a persistent bottom sheet.
+   */
+  modal: PropTypes.bool,
+  /**
+   *
+   */
+};
 
 export default withStyles(styleSheet)(BottomSheet);
